698e34a646f774e945a8c39f69235303188789e5
nguyenkhoa <nguyenkhoa0721@gmail.com>
2021-09-24 09:58:24 +0700

    add readme

---
+++ readme.md
@@ -0,0 +1,41 @@
+# Setup
+```
+make
+```
+# Working with stack
+## Viewing logs
+```
+docker logs [container_name] [options]
+```
+
+Exampe: Xem logs của api
+
+```
+docker logs api -f
+```
+
+## Restarting a single contrainer
+```
+docker restart [container_name]
+```
+
+## Restarting entire stack
+
+```
+make
+```
+
+## Running a command inside a container
+
+Example:
+
+```
+docker exec -it debion_tp_api /bin/ash
+
+(inside container shell)
+npm install abcxyz
+
+(to leave container shell)
+Ctrl+P+Q
+````
+
--- utils/response.js
+++ utils/response.js
@@ -1,5 +1,4 @@
 exports.resSuccess = async (response, data, status = 200) => {
-    data = await changePath(data)
     const responseData = {
         status,
         message: 'success',
@@ -14,21 +13,4 @@ exports.resError = function (response, message, status = 422) {
     }
     const responseData = { status, error: message }
     return response.status(status).send(responseData)
-}
-
-changePath = (data) => {
-    let str = JSON.stringify(data)
-    try {
-        str = str.replace(/"path":"/g, `"path":"${process.env.CDN_URL_PREFIX + process.env.API_PREFIX}`)
-        str = str.replace(/"logoPath":"/g, `"logoPath":"${process.env.CDN_URL_PREFIX + process.env.API_PREFIX}`)
-        str = str.replace(/"coverPath":"/g, `"coverPath":"${process.env.CDN_URL_PREFIX + process.env.API_PREFIX}`)
-        str = str.replace(/"model3d":"/g, `"model3d":"${process.env.CDN_URL_PREFIX + process.env.API_PREFIX}`)
-        str = str.replace(/"boqFilePath":"/g, `"boqFilePath":"${process.env.CDN_URL_PREFIX + process.env.API_PREFIX}`)
-        str = str.replace(/"quotationFilePath":"/g, `"quotationFilePath":"${process.env.CDN_URL_PREFIX + process.env.API_PREFIX}`)
-        return JSON.parse(str)
-    }
-    catch (err) {
-        console.log(err)
-        return data
-    }
 }
