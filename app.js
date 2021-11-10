b6d82edcbbb988afaa11f4b3011cfd43912a1949
nguyenkhoa <nguyenkhoa0721@gmail.com>
2021-11-10 20:25:27 +0700

    cors

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ const xss = require("xss-clean");
 
 const app = express();
 
+const whitelist = process.env.ACCESS_CONTROL_ALLOW_ORIGIN.split(",");
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) callback(null, true);
+    else if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"), false);
+    }
+  },
+  credentials: true,
+};
+
 //app use
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(helmet());
