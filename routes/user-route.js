956932bf6248ef4f63808a14281a0adbf92edbff
nguyenkhoa <nguyenkhoa0721@gmail.com>
2021-12-21 08:55:02 +0700

    add invite project

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const route = express.Router();
 const authController = require("../controllers/auth-controller");
+const projectController = require("../controllers/project-controller");
 
 route.post("/signup", authController.signup);
 route.post("/login", authController.login);
 //route.get("/test_token",authController.isLogged);
-module.exports = route; 
+
+route.get("/projects", authController.isLogged, projectController.getAll);
+route
+  .route("/project/:id")
+  .get(authController.isLogged, projectController.getOne)
+  .post(authController.isLogged, projectController.create)
+  .delete(authController.isLogged, projectController.delete);
+
+route
+  .route("/project/:id/invite")
+  .get(authController.isLogged, projectController.generateInviteUrl)
+  .post(authController.isLogged, projectController.join);
+module.exports = route;
