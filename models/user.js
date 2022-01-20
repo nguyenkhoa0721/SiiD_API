f89b5be8f8b9e717d306650f2f1f0eb27958a6ce
nguyenkhoa <nguyenkhoa0721@gmail.com>
2022-01-20 15:16:30 +0700

    abc

--- models/user.js
+++ models/user.js
@@ -3,35 +3,38 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    lowercase: true,
-    required: [true, "can't be blank"],
-    match: [/^[a-zA-Z0-9]+$/, "is invalid"],
-    index: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      index: true,
+      unique: true,
+    },
+    phone: String,
+    name: {
+      type: String,
+      required: [true, "name required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email required"],
+      unique: true,
+      lowercase: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    birth: Date,
+    avatar: String,
   },
-  phone: String,
-  name: {
-    type: String,
-    required: [true, "name required"],
-  },
-  email: {
-    type: String,
-    required: [true, "email required"],
-    unique: true,
-    lowercase: true,
-    validate: validator.isEmail,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  birth: Date,
-  avatar: String,
-},{timestamps: true});
+  { timestamps: true }
+);
 
 userSchema.set("toObject", { virtuals: true });
 userSchema.set("toJSON", { virtuals: true });
@@ -44,10 +47,7 @@ userSchema.pre("save", async function (next) {
 });
 
 //hàm để check xem password nhập nào có giôngs trong database
-userSchema.methods.correctPassword = async function (
-  candidatePassword,
-  userPassword
-) {
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
