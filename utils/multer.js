984e46104f77a0622b7479921e161b811f7df6cf
nguyenkhoa <nguyenkhoa0721@gmail.com>
2022-01-25 22:03:42 +0700

    add psd ext

--- utils/multer.js
+++ utils/multer.js
@@ -1,26 +1,27 @@
-const multer = require('multer');
-const slugify = require('slugify');
-const storage =  multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, req.dirname1);
-    },
-    filename: function(req, file, cb) {
-      let tmp = slugify(file.originalname);
-      cb(null, Date.now() + '-' + tmp);
-    }
+const multer = require("multer");
+const slugify = require("slugify");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, req.dirname1);
+  },
+  filename: function (req, file, cb) {
+    let tmp = slugify(file.originalname);
+    cb(null, Date.now() + "-" + tmp);
+  },
 });
 const fileFilter = (req, file, cb) => {
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.minetype === "psd"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 exports.upload = multer({
-    storage: storage,
-    fileFilter: fileFilter
+  storage: storage,
+  fileFilter: fileFilter,
 });
